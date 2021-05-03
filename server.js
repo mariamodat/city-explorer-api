9552ac1f226960cf3dbc3145dd1c32b008a383b9
mariamodat <mariamodat0@gmail.com>
2021-05-03 19:59:38 +0300

    API request

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+let weatherData = require('./assests/weather.json');
+const send = require('send');
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// });
+// app.get('/' , (req,res) => res.send('this is Mariam'));
+app.get('/' , (req,res)=> {
+    res.send(weatherData);
 });
  
-app.listen(3002);
+app.listen(5000);
