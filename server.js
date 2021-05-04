15a56102f9ce97452f700e803685d97e2346f1ec
mariamodat <mariamodat0@gmail.com>
2021-05-04 11:42:04 +0300

    added const

--- server.js
+++ server.js
@@ -6,22 +6,29 @@ let weatherData = require('./weather.json');
 // const request = require('request');
 // const { request } = require('express');
 // const { default: Weather } = require('./frontend/src/weather');
- 
+
 // app.get('/', function (req, res) {
 //   res.send('Hello World')
 // });
 // app.get('/' , (req,res) => res.send('this is Mariam'));
 app.use(cors());
-app.get('/' , (req,res)=> {
-    res.send(weatherData.data);
-    // request()
-    // const dataFile=require(`./weather.json`);
-    // let dataArr= []; 
-    // dataFile.data.forEach(item=>{
-    //     let weatherData= new Weather(item);
-    //     dataArr.push(weatherData);
-    //     res.send(dataArr);
-    // })
+app.get('/', (req, res) => {
+  res.send(weatherData.data);
+  // request()
+
+  let dataArr= []; 
+  weatherData.data.forEach(item=>{
+      let weatherData= new Weather(item);
+      dataArr.push(weatherData);
+      res.send(dataArr);
+  })
 });
- 
+
+function Weather(item){
+    
+  this.forecast=item.weather.description;
+  this.time=item.datetime;
+
+}
+
 app.listen(5000);
